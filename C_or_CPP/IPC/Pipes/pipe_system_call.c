5a9f4dc1388a29f4725abf2532f379d6a1760d66
rajasekhar0001 <bodeddularajasekharreddy1111@gmail.com>
2024-04-26 20:52:57 +0530

    pipe

---
+++ C_or_CPP/IPC/Pipes/pipe_system_call.c
@@ -0,0 +1,45 @@
+/**
+ * pipe
+ * 
+ * returns 0 on success and -1 on failure
+*/
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main() {
+    int pipefd[2];
+
+    /**
+     * pipefd[0] -> file descriptor to read into pipe or refers to the read end of the pipe
+     * pipefd[1] -> file descriptor to write from pipe or refers to the  write end of the pipe
+    */
+    if (pipe(pipefd)==-1) {
+        perror("pipe");
+        exit(0);
+    }
+
+    int pid = fork();
+    if (pid > 0) {
+        printf("Parent process\n");
+        // write access
+        // Consider parent to writie into pipe
+        // remote read file descriptor
+        close(pipefd[0]);
+        write(pipefd[1], "Hello Welcome", 14);
+
+    }
+    else if (pid ==0) {
+        printf("Child process\n");
+        // read access
+        // close the write file descriptor
+        close(pipefd[1]);
+        char buf[15];
+        read(pipefd[0], buf, 15);
+        printf("Buffer: %s\n", buf);
+
+    }
+    else {
+        perror("fork");
+    }
+}
