b9052b074baa8d6fbd629799ee88d98ce47b5cd8
rajasekhar0001 <bodeddularajasekharreddy1111@gmail.com>
2024-09-15 23:13:40 +0530

    streams in cpp

---
+++ C_or_CPP/CPP/From_Video/ip_op.cpp
@@ -0,0 +1,16 @@
+// Different types of streams in cpp
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello using cout\n";   // prints data to the console
+
+    cerr << "Hello using cerr\n";   // prints error message to the console
+
+    clog << "Hello using clog\n";   // prints log messages to the console
+
+    int n;
+
+    cin >> n ;  // reads data from the terminal
+}
