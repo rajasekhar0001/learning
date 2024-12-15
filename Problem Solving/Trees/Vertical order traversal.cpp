57061f5618f74d1555ce8fbca084071284864eab
rajasekhar0001 <bodeddularajasekharreddy1111@gmail.com>
2024-12-15 21:27:25 +0530

    vertical order traversal of a tree link in leetcode

---
+++ Problem Solving/Trees/Vertical order traversal.cpp
@@ -0,0 +1,20 @@
+/**
+ * Ex: Vertical order traversal
+ 
+             2
+           /   \
+          3     4
+        /   \  /  \
+        5   6  7   8
+    
+    o/p:   5
+           3
+           2 6 7 
+           4
+           8
+ */
+
+/**
+ * Link: https://leetcode.com/problems/vertical-order-traversal-of-a-binary-tree/submissions/1479506913/
+ * * CHeck in Leetcode
+ */
