67385685cc0c64a86f8f8dccd1416034ecef9443
rajasekhar0001 <bodeddularajasekharreddy1111@gmail.com>
2024-12-22 18:47:28 +0530

    LCA in a binary tree

---
+++ Problem Solving/Trees/LCA_in_binary_tree.cpp
@@ -0,0 +1,8 @@
+/**
+ * Least CommonAncestor in binary tree
+ * WIth O(n) time comlexity and O(N) space complexity
+ */
+
+/**
+ * Link: https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
+ */
